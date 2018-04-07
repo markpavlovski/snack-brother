ffa68f7d5e5f0b40bf3c49bb66d53bcbb68a2086
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-07 10:16:29 -0700

    added message rerouting to slack

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const axios = require('axios')
 
 app.disable('x-powered-by')
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   const message = process.env.MESSAGE || 'Please provide an environment variable named MESSAGE'
-  res.json({ message })
+  axios.post(`https://hooks.slack.com/services/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`, {text: `${req.body.text}`})
+  .then(function (result) {
+
+    console.log("\nNew Request")
+  })
+  res.json(req.body)
 })
 
-const listener = () => `Listening on port ${port}!`
+const listener = () => console.log(`Listening on port ${port}!`)
 app.listen(port, listener)
 
 module.exports = app
