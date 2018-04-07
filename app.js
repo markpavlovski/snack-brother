4a8d402889e91796223443c2acd00d660d5dce8c
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-06 22:30:10 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.disable('x-powered-by')
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  const message = process.env.MESSAGE || 'Please provide an environment variable named MESSAGE'
+  res.json({ message })
+})
+
+const listener = () => `Listening on port ${port}!`
+app.listen(port, listener)
+module.exports = app
