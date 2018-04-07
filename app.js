9acb508861f9c1744b3a66d2ce3d1f145c479a06
Mark Pavlovski <markpavlovski@gmail.com>
2018-04-07 10:32:17 -0700

    changed request body to reqest params

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ app.disable('x-powered-by')
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 app.use(bodyParser.json())
 
-app.post('/', (req, res) => {
+app.get('/:text', (req, res) => {
   const message = process.env.MESSAGE || 'Please provide an environment variable named MESSAGE'
-  axios.post(`https://hooks.slack.com/services/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`, {text: `${req.body.text}`})
+  axios.post(`https://hooks.slack.com/services/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`, {text: `${req.params.text}`})
   .then(function (result) {
 
-    console.log(`\nNew Request - ${req.body.text}`)
+    console.log(`\nNew Request - ${req.params.text}`)
   })
-  res.json(req.body)
+  res.json(req.params.text)
 })
 
 const listener = () => console.log(`Listening on port ${port}!`)
